5dd3ad1a1dcf3d21a5c988dfdcc84b03c8da2210
willemchua <willem.chua23@gmail.com>
2017-10-03 16:26:22 +0700

    add chat and request components

--- ess-app/src/app/dashboard/dashboard.module.ts
+++ ess-app/src/app/dashboard/dashboard.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from './../shared/material/material.module';
 import { DashboardComponent } from './dashboard.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ChatComponent } from './chat/chat.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MaterialModule
   ],
   declarations: [
     DashboardComponent,
-    MenuComponent
+    MenuComponent,
+    ChatComponent,
+    RequestComponent
   ]
 })
 export class DashboardModule { }
