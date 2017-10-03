623e7dfdb954b7a4109c078bc11483f73f688c7d
willemchua <willem.chua23@gmail.com>
2017-10-03 10:13:26 +0700

    add module imports

--- ess-app/src/app/app.module.ts
+++ ess-app/src/app/app.module.ts
@@ -1,22 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { MaterialModule } from './shared/material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { InjectionService } from './core/utilities/injection.service';
+import { NetworkModule } from './core/network/network.module';
+import { StorageModule } from './core/storage/storage.module';
+import { reducer } from './app.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
     LoginModule,
     DashboardModule,
-    MaterialModule
+    MaterialModule,
+    NetworkModule,
+    StorageModule,
+    StoreModule.provideStore(reducer)
   ],
-  providers: [],
+  providers: [
+    InjectionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
