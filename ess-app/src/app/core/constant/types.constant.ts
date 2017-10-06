11cfd26c867ca8ff80edebec9b2571bd9688503e
willemchua <willem.chua23@gmail.com>
2017-10-06 16:22:20 +0700

    change declaration of leave types

--- ess-app/src/app/core/constant/types.constant.ts
+++ ess-app/src/app/core/constant/types.constant.ts
@@ -1,15 +1,13 @@
-export const TYPES = {
-    REIMBURSEMENT: [
-        'Medical',
-        'Taxi',
-        'Client Meeting'
-    ],
-
-    LEAVE: [
-        'Sick Leave',
-        'Annual Leave',
-        'Unpaid Leave',
-        'Partial Day Leave'
-    ],
-
+export const LEAVE_TYPES = {
+    SICK_LEAVE: 'Sick Leave',
+    UNPAID_LEAVE: 'Unpaid Leave',
+    ANNUAL_LEAVE: 'Annual Leave',
+    PARTIAL_DAY_LEAVE: 'Partial Day Leave'
 }
+
+export const REIMBURSEMENT_TYPES = {
+    MEDICAL_REIMBURSEMENT: 'Medical',
+    TRANSPORT_REIMBURSEMENT: 'Transportation',
+    MEETING_EXPENSES_REIMBURSEMENT: 'Meeting Expenses'
+}
+
