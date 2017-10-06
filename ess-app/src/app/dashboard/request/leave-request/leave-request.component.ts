9c657f53abbf7a927cfe9ac2e38addf7d321624a
willemchua <willem.chua23@gmail.com>
2017-10-06 16:23:21 +0700

    create simple layout for leave request

--- ess-app/src/app/dashboard/request/leave-request/leave-request.component.ts
+++ ess-app/src/app/dashboard/request/leave-request/leave-request.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { values } from 'lodash';
+
+import { LEAVE_TYPES } from './../../../core/constant';
+import { DashboardAction } from './../../shared/dashboard.action';
+
 @Component({
   selector: 'app-leave-request',
   templateUrl: './leave-request.component.html',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaveRequestComponent implements OnInit {
 
-  constructor() { }
+  leaveTypes = values(LEAVE_TYPES);
+  singleDatepicker: boolean = false;
+  sliderValue: number = 0;
+
+  selectedLeaveType;
+
+  constructor(private store: Store<any>) {
+    this.store.dispatch({
+      type: DashboardAction.CHANGE_TITLE,
+      payload: 'Leave'
+    })
+  }
 
   ngOnInit() {
+  }
+
+  doStuff() {
+    if (this.selectedLeaveType === LEAVE_TYPES.PARTIAL_DAY_LEAVE) {
+      this.singleDatepicker = true;
+    } else {
+      this.singleDatepicker = false;
+    }
+  }
+
+  doWeirdStuff(event: any) {
+    this.sliderValue = event.value;
+    console.log(this.sliderValue);
   }
 
 }
