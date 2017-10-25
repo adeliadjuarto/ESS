5eeba08cb92996747bb6aab12c644c5259186e27
adeliadjuarto <dj.adelia18@gmail.com>
2017-10-25 14:32:48 +0700

    Add GoogleCalendarService and do authentication

--- ess-be/src/main/java/app/cms/controller/EventController.java
+++ ess-be/src/main/java/app/cms/controller/EventController.java
@@ -1,5 +1,6 @@
 package app.cms.controller;
 
+import app.cms.service.GoogleCalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class EventController {
+    @Autowired
+    private GoogleCalendarService googleCalendarService;
+
     @RequestMapping("/event")
     public String events(Model model) throws Exception {
         model.addAttribute("currPage", "event");
         return "event/index";
+    }
+    @RequestMapping("/event/save")
+    public String testEvents(Model model) throws Exception {
+
+        return "success";
     }
 }
