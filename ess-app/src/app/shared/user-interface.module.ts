1cea4d19fee350479d03c3a943c745b894e5f0fe
willemchua <willem.chua23@gmail.com>
2017-11-02 14:56:26 +0700

    add pdf viewer component

--- ess-app/src/app/shared/user-interface.module.ts
+++ ess-app/src/app/shared/user-interface.module.ts
@@ -2,8 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
+
 import { NotificationModule } from './notification/notification.module';
 import { CircularRangeSliderComponent } from './circular-range-slider/circular-range-slider.component';
+import { PdfViewersComponent } from './pdf-viewer/pdf-viewer.component';
 
 @NgModule({
   imports: [
@@ -12,10 +15,13 @@ import { CircularRangeSliderComponent } from './circular-range-slider/circular-r
   ],
   exports: [
     NotificationModule,
-    MaterialModule
+    MaterialModule,
+    PdfViewersComponent
   ],
   declarations: [
-    CircularRangeSliderComponent
+    CircularRangeSliderComponent,
+    PdfViewerComponent,
+    PdfViewersComponent
   ]
 })
 export class UIModule { }
