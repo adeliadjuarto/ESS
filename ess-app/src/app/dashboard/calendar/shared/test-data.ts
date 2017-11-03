3ea5979bdd6bae09ff4335acb6b6c93ce475b77a
willemchua <willem.chua23@gmail.com>
2017-11-03 11:46:31 +0700

    add sample data

---
+++ ess-app/src/app/dashboard/calendar/shared/test-data.ts
@@ -0,0 +1,56 @@
+export const TestData = [
+    {
+        attendees: [
+            {
+                displayName: 'Willem Chua',
+                email: 'willem.chua23@gmail.com',
+                responseStatus: 'needsAction'
+            }
+        ],
+        created: {
+            value: 1509608120000,
+            dateOnly: false,
+            timeZoneShift: 0
+        },
+        creator: {
+            displayName: 'Adelia Djuarto',
+            email: 'dj.adelia18@gmail.com'
+        },
+        description: 'lalalal 123 lorem ipsum',
+        end: {
+            dateTime: {
+                value: 1509613200000,
+                dateOnly: false,
+                timeZoneShift: 420
+            }
+        },
+        etag: '\"3019217743344000\"',
+        htmlLink: 'https://www.google.com/calendar/event?eid=MDVmdmFnM21uZmpkNGc1b2hhYTNzMjBhMmcgMGx2cTAyc2o4OHBpbmVoM2M1OHRncm82M3NAZw',
+        iCalUID: '05fvag3mnfjd4g5ohaa3s20a2g@google.com',
+        id: '05fvag3mnfjd4g5ohaa3s20a2g',
+        kind: 'calendar#event',
+        organizer: {
+            displayName: 'ESS',
+            email: '0lvq02sj88pineh3c58tgro63s@group.calendar.google.com',
+            self: true
+        },
+        reminders: {
+            useDefault: true
+        },
+        sequence: 0,
+        start: {
+            dateTime: {
+                value: 1509609600000,
+                dateOnly: false,
+                timeZoneShift: 420
+            }
+        },
+        status: 'confirmed',
+        summary: 'Test',
+        updated: {
+            value: 1509608871672,
+            dateOnly: false,
+            timeZoneShift: 0
+        }
+    }
+]
