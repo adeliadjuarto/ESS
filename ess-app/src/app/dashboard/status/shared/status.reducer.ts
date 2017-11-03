ea8d05cc1a6fe47cf74c67f7b797758a52cacf00
willemchua <willem.chua23@gmail.com>
2017-11-03 18:11:25 +0700

    add status service and reducer

---
+++ ess-app/src/app/dashboard/status/shared/status.reducer.ts
@@ -0,0 +1,48 @@
+import { ActionReducer, Action, combineReducers } from '@ngrx/store';
+import { Status } from './status.model';
+
+import { StatusAction as Actions } from './status.action';
+
+export interface StatusState {
+    approvalItems: Status[];
+    error;
+    loading: boolean;
+    success: boolean;
+}
+
+export const initialState: StatusState = {
+    approvalItems: [],
+    error: null,
+    loading: false,
+    success: false
+};
+
+export function reducer(state: StatusState = initialState, action: Action) {
+    switch (action.type) {
+        case Actions.FETCH_STATUS_BEGIN:
+            return Object.assign({}, state, { approvalStatus: {
+                approvalItems: state.approvalItems,
+                error: null,
+                loading: true,
+                success: false,
+            } });
+        case Actions.FETCH_STATUS_SUCCESS:
+            return Object.assign({}, state, { approvalStatus: {
+                approvalItems: action.payload,
+                error: null,
+                loading: false,
+                success: true,
+            } });
+        case Actions.FETCH_STATUS_ERROR:
+            return Object.assign({}, state, { approvalStatus: {
+                approvalItems: state.approvalItems,
+                error: action.payload,
+                loading: false,
+                success: false,
+            } });
+        case Actions.RESET:
+            return initialState;
+        default:
+            return state;
+    }
+};
---
+++ ess-app/src/app/dashboard/status/shared/status.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { DataService } from './../../../core/data.service';
+import { ApiService } from './../../../core/network/api.service';
+import { ENDPOINT } from './../../../core/constant/index';
+import { Request } from './../../../core/network/request.interface';
+import { Response } from './../../../core/network/response.interface';
+import { ResponseType } from './../../../core/network/serializer/response-type.abstract';
+import { Status } from './status.model';
+import { StatusAction as Actions } from './status.action';
+
+@Injectable()
+export class StatusService extends DataService<Status> {
+
+  constructor(private store: Store<any>,
+              apiService: ApiService) {
+    super(apiService);
+    super.setEndpoint(ENDPOINT.REQUEST.LEAVE, Status);
+  }
+
+  fetchApprovals(endpointType, param) {
+    this.store.dispatch({type: Actions.FETCH_STATUS_BEGIN});
+
+    this.fetchApprovalStatus(endpointType, param).subscribe(
+      this.fetchApprovalStatusSuccess.bind(this),
+      this.fetchApprovalStatusError.bind(this)
+    );
+  }
+
+  fetchApprovalStatus(endpoint, param?) {
+    return this.apiService.get(endpoint, (request: Request) => {
+                              request.setQuery(param);
+                          })
+                          .map((response: Response) => response.to(ResponseType.list));
+  }
+
+  fetchApprovalStatusSuccess(result) {
+    this.store.dispatch({
+      type: Actions.FETCH_STATUS_SUCCESS,
+      payload: result.content,
+    });
+  }
+
+  fetchApprovalStatusError(result) {
+    this.store.dispatch({
+      type: Actions.FETCH_STATUS_ERROR,
+      payload: result,
+    });
+  }
+
+}
