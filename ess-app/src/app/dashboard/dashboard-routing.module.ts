36e7e8675321090e11dd5e0265cc10064d504521
willemchua <willem.chua23@gmail.com>
2017-11-06 17:48:57 +0700

    add guard in dashboard routing

--- ess-app/src/app/dashboard/dashboard-routing.module.ts
+++ ess-app/src/app/dashboard/dashboard-routing.module.ts
@@ -14,6 +14,7 @@ import { StatusComponent } from './status/status.component';
 const routes: Routes = [
   {
     path: PATH.EMPTY,
+    canActivateChild: [AuthenticatedGuard],
     component: DashboardComponent,
     children: [
       {
