3e7df55be5e0632f10c2ba0755e6fdc12eb0c2c7
willemchua <willem.chua23@gmail.com>
2017-11-09 15:30:02 +0700

    fix status reducer

--- ess-app/src/app/dashboard/status/shared/status.reducer.ts
+++ ess-app/src/app/dashboard/status/shared/status.reducer.ts
@@ -20,26 +20,26 @@ export const initialState: StatusState = {
 export function reducer(state: StatusState = initialState, action: Action) {
     switch (action.type) {
         case Actions.FETCH_STATUS_BEGIN:
-            return Object.assign({}, state, { approvalStatus: {
+            return Object.assign({}, state, { 
                 approvalItems: state.approvalItems,
                 error: null,
                 loading: true,
                 success: false,
-            } });
+            });
         case Actions.FETCH_STATUS_SUCCESS:
-            return Object.assign({}, state, { approvalStatus: {
+            return Object.assign({}, state, {
                 approvalItems: action.payload,
                 error: null,
                 loading: false,
                 success: true,
-            } });
+            });
         case Actions.FETCH_STATUS_ERROR:
-            return Object.assign({}, state, { approvalStatus: {
+            return Object.assign({}, state, {
                 approvalItems: state.approvalItems,
                 error: action.payload,
                 loading: false,
                 success: false,
-            } });
+            });
         case Actions.RESET:
             return initialState;
         default:
