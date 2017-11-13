5a029f902d0840e7ea1229b17638a8cebae7743e
willemchua <willem.chua23@gmail.com>
2017-11-13 18:09:55 +0700

    add chatAPIcontroller

---
+++ ess-be/src/main/java/app/cms/controller/api/ChatApiController.java
@@ -0,0 +1,26 @@
+package app.cms.controller.api;
+
+import app.cms.model.Chat;
+import app.cms.model.Document;
+import app.cms.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by willemchua on 11/13/17.
+ */
+@RestController
+public class ChatApiController {
+
+    @Autowired
+    private ChatService chatService;
+
+    @RequestMapping("/chat")
+    public Chat getChat(
+            @RequestParam("m") String message
+    ) throws Exception {
+        return chatService.chat(message);
+    }
+}
