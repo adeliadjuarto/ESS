4598675689899d602ea3052e769840cf6ad501c4
willemchua <willem.chua23@gmail.com>
2017-12-11 14:35:43 +0700

    add payroll reducer

--- ess-app/src/app/app.reducer.ts
+++ ess-app/src/app/app.reducer.ts
@@ -5,15 +5,18 @@ import { environment } from '../environments/environment';
 
 import * as fromLogin from './login/shared/login.reducer';
 import * as fromDashboard from './dashboard/shared/dashboard.reducer';
+import * as fromPayroll from './dashboard/payroll/shared/payroll.reducer';
 
 export interface AppState {
     loginState: fromLogin.LoginState;
     dashboardState: fromDashboard.DashboardState;
+    payrollState: fromPayroll.PayrollState;
 }
 
 const reducers = {
     loginState: fromLogin.reducer,
-    dashboardState: fromDashboard.reducer
+    dashboardState: fromDashboard.reducer,
+    payrollState: fromPayroll.reducer
 };
 
 export const prodReducer: ActionReducer<AppState> = combineReducers(reducers);
---
+++ ess-app/src/app/dashboard/payroll/shared/payroll.reducer.ts
@@ -0,0 +1,34 @@
+import { Action } from '@ngrx/store';
+
+import { PayrollAction } from './payroll.action';
+import { Payroll } from './payroll.model';
+
+export interface PayrollState {
+    latestPayroll: Payroll;
+    currentPayroll: Payroll;
+}
+
+export const initialState: PayrollState = {
+    latestPayroll: null,
+    currentPayroll: null
+};
+
+export function reducer(state: PayrollState = initialState, action: Action) {
+  switch (action.type) {
+    case(PayrollAction.FETCH_LATEST_PAYROLL_SUCCESS):
+        return Object.assign({}, state, {
+            latestPayroll: action.payload,
+            currentPayroll: state.currentPayroll
+        });
+    case(PayrollAction.FETCH_PAYROLL_SUCCESS):
+        return Object.assign({}, state, {
+            latestPayroll: state.latestPayroll,
+            currentPayroll: action.payload
+        })
+
+    default:
+        return state;
+
+  }
+
+}
