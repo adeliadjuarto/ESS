95778be0513008061d52f8cc6fe36a72f42f2a60
adeliadjuarto <dj.adelia18@gmail.com>
2017-12-15 17:38:19 +0700

    Create leave request from chatbot (without file upload)

--- ess-be/src/main/java/app/cms/service/ChatService.java
+++ ess-be/src/main/java/app/cms/service/ChatService.java
@@ -5,26 +5,23 @@ import app.cms.controller.api.OvertimeRequestApiController;
 import app.cms.controller.api.PayrollApiController;
 import app.cms.controller.api.ReimbursementRequestApiController;
 import app.cms.model.*;
-import app.cms.repository.PayrollRepository;
-import app.cms.repository.UserRepository;
+import app.cms.repository.LeaveRequestRepository;
+import app.cms.repository.RequestSessionRepository;
+import app.cms.repository.RequestTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static app.cms.commons.MenuConstants.*;
+import static org.springframework.util.StringUtils.isEmpty;
 
 /**
  * Created by willemchua on 11/13/17.
  */
 @Service
 public class ChatService {
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private AuthenticationService authService;
@@ -41,38 +38,53 @@ public class ChatService {
     @Autowired
     private ReimbursementRequestApiController reimbursementRequestApiController;
 
+    @Autowired
+    private RequestTypeRepository requestTypeRepository;
+
+    @Autowired
+    private LeaveRequestRepository leaveRequestRepository;
+
+    @Autowired
+    private RequestSessionRepository requestSessionRepository;
+
+    @Autowired
+    private ChatState chatState;
+
     public Chat chat(String message) throws Exception {
-        if(MenuConstants.contains(message)) {
-            switch(message) {
-                case REQUEST:
-                    return chatRequest();
-                case STATUS:
-                    return chatStatus();
-                case REQUEST_LEAVE:
-                    return chatRequestLeave();
-                case REQUEST_OVERTIME:
-                    return chatRequestOvertime();
-                case REQUEST_REIMBURSEMENT:
-                    return chatRequestReimbursement();
-                case EVENTS:
-                    return chatEvents();
-                case LEAVE_BALANCE:
-                    return chatLeaveBalance();
-                case PAYROLL_STATUS:
-                    return chatPayrollStatus();
-                case LEAVE_STATUS:
-                    return chatLeaveStatus();
-                case OVERTIME_STATUS:
-                    return chatOvertimeStatus();
-                case REIMBURSEMENT_STATUS:
-                    return chatReimbursementStatus();
-                case HOME:
-                    return newChat();
-                default:
-                    return new Chat(Arrays.asList("Menu belum dibuat"));
-            }
+        String menu;
+        String currentState = chatState.getState();
+        if(isEmpty(currentState) || message.equals(HOME)) {
+            menu = message;
         } else {
-            return chatNonPredefined(message);
+            menu = currentState;
+        }
+        switch(menu) {
+            case REQUEST:
+                return chatRequest();
+            case STATUS:
+                return chatStatus();
+            case REQUEST_LEAVE:
+                return chatRequestLeave(message);
+            case REQUEST_OVERTIME:
+                return chatRequestOvertime();
+            case REQUEST_REIMBURSEMENT:
+                return chatRequestReimbursement();
+            case EVENTS:
+                return chatEvents();
+            case LEAVE_BALANCE:
+                return chatLeaveBalance();
+            case PAYROLL_STATUS:
+                return chatPayrollStatus();
+            case LEAVE_STATUS:
+                return chatLeaveStatus();
+            case OVERTIME_STATUS:
+                return chatOvertimeStatus();
+            case REIMBURSEMENT_STATUS:
+                return chatReimbursementStatus();
+            case HOME:
+                return newChat();
+            default:
+                return new Chat(Arrays.asList("Menu belum dibuat"));
         }
     }
 
@@ -135,8 +147,63 @@ public class ChatService {
         return new Chat(messages, buttons);
     }
 
-    private Chat chatRequestLeave() {
-        return new Chat();
+    private Chat chatRequestLeave(String input) {
+        if (!chatState.getState().equals(REQUEST_LEAVE)) {
+            chatState.setIsInitiation(true);
+        } else {
+            chatState.setIsInitiation(false);
+        }
+        Boolean isValid = true;
+        String message = "Data lengkap, pengajuan sudah dibuat!";
+        String type = "leave";
+        List<String> neededFields = LEAVE_REQUEST_FIELDS;
+
+        User user = authService.getCurrentUser();
+        RequestSession requestSession = requestSessionRepository
+                .findFirstByUserAndType(user, type);
+        if (requestSession == null) {
+            requestSession = new RequestSession(user, type);
+            message = "Request session created";
+        } else {
+            if (!chatState.getIsInitiation()) {
+                requestSession.addData(chatState.getField(), input);
+            }
+            for (String field : neededFields) {
+                String value = requestSession.getValue(field);
+                if (isEmpty(value)) {
+                    message = "Tolong masukkan " + field;
+                    isValid = false;
+                    chatState.setField(field);
+                    break;
+                }
+            }
+        }
+
+        requestSessionRepository.save(requestSession);
+
+        if (isValid) {
+            //save data to repository & delete this request session
+            String title = requestSession.getValue("judul");
+            String description = requestSession.getValue("keterangan");
+            Long start = Long.parseLong(requestSession.getValue("tanggal mulai"));
+            Long end = Long.parseLong(requestSession.getValue("tanggal selesai"));
+
+            RequestType requestType = requestTypeRepository.findOne(
+                    Long.parseLong(requestSession.getValue("jenis pengajuan cuti"))
+            );
+            LeaveRequest leaveRequest = new LeaveRequest(
+                    title, description, start, end, requestType, user);
+            leaveRequestRepository.save(leaveRequest);
+            requestSessionRepository.delete(requestSession);
+            resetChatState();
+        }
+
+        List<String> messages = Arrays.asList(message);
+        List<String> buttons = Arrays.asList(HOME);
+
+
+        chatState.setState(REQUEST_LEAVE);
+        return new Chat(messages, buttons);
     }
 
     private Chat chatRequestOvertime() {
@@ -184,6 +251,13 @@ public class ChatService {
                 EVENTS,
                 LEAVE_BALANCE
         );
+        resetChatState();
         return new Chat(messages, buttons);
+    }
+
+    private void resetChatState() {
+        chatState.setState("");
+        chatState.setField("");
+        chatState.setIsInitiation(true);
     }
 }
