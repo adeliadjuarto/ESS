f0035896fdb469b97b480559f3945d1e12ca5fd3
willemchua <willem.chua23@gmail.com>
2017-12-20 15:20:00 +0700

    add bg-sync and notification

--- ess-app/src/sw.js
+++ ess-app/src/sw.js
@@ -1,10 +1,75 @@
 importScripts('workbox-sw.prod.v2.1.0.js');
+importScripts('assets/lodash.min.js');
 
 const workboxSW = new WorkboxSW({skipWaiting: true, clientsClaim: true});
 
 self.addEventListener('activate', (event) => {
     console.log('Service Worker activated');
 });
+
+self.importScripts('assets/idb-keyval-min.js');
+
+self.addEventListener('sync', event => {
+  if (event.tag === 'pwa-request-sync') {
+    event.waitUntil(getRequestsFromOutbox()
+      .then(requests => Promise.all(mapAndSend(requests)))
+      .catch(err => console.log(err))
+      .then(response => removeRequestsFromOutBox(response))
+    );
+  }
+});
+
+function getRequestsFromOutbox() {
+  const key = 'pwa-sync';
+  return idbKeyval.get(key).then(values => {
+    values = values || [];
+    const messages = values || [];
+    return messages;
+  });
+}
+
+function mapAndSend(requests) {
+  return requests.map(
+    request => sendMessage(request)
+      .then(response => {
+        registration.showNotification('Request Sent!');
+        return response;
+      })
+      .catch(err => console.log(err)));
+}
+
+function sendMessage(requestObj) {
+  let url = requestObj.url;
+  let req = requestObj.request;
+  var request = new FormData();
+  _.mapKeys(req, (value, mapKey) =>{request.append(mapKey, value)});
+
+  const headers = {
+    'X-Requested-With': 'XMLHttpRequest',
+  };
+
+  const msg = {
+    method: 'POST',
+    body: request,
+    credentials: 'include',
+    headers: headers
+  };
+
+  return fetch(url, msg).then((response) => {
+    console.log('message sent!');
+    return response;
+  });
+}
+
+function removeRequestsFromOutBox(response) {
+  // If the first worked,let's assume for now they all did
+  if (response && response.length && response[0] && response[0].status === 200) {
+    return idbKeyval.clear()
+      .then(() => console.log('messages removed from outbox'))
+      .catch(err => console.log('unable to remove messages from outbox'));
+  }
+  return Promise.resolve(true);
+}
 
 workboxSW.precache([]);
 
