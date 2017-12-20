67fe1f7cfc7534a464a526a6848a99876b202cb8
willemchua <willem.chua23@gmail.com>
2017-12-20 15:19:53 +0700

    ask for notification permission

--- ess-app/src/app/app.component.ts
+++ ess-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    Notification.requestPermission().then(
+      result => console.log(result)
+    )
+  }
 }
