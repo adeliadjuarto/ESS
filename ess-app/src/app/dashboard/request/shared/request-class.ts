d1caac1c5d106bf0329792553214c53bce5d75a8
willemchua <willem.chua23@gmail.com>
2018-01-12 15:53:48 +0700

    add to request class

--- ess-app/src/app/dashboard/request/shared/request-class.ts
+++ ess-app/src/app/dashboard/request/shared/request-class.ts
@@ -3,6 +3,13 @@ import { IRequest } from './request.interface';
 export abstract class RequestClass {
 
     protected abstract request: any;
+    protected abstract userId: string;
+    protected abstract requestConfirm: boolean;
+    protected abstract errorMessage: string;
+    protected abstract pendingRequests: number;
+    protected abstract fileName: string;
+
+    protected abstract confirmRequest();
     protected abstract submitRequest();
     protected abstract requestValid();
     protected abstract fileChange(event: any);
